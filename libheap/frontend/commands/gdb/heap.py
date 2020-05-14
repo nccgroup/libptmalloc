9fa451c6a515a5e04c84e4e1455d1c1160eb448f
Aaron Adams <aaron.adams@nccgroup.com>
2020-05-14 17:25:53 +0800

    basic changes to get it loading from gdb without installing with pip
    also changes to at least get the basic keep command to find an arena

--- libheap/frontend/commands/gdb/heap.py
+++ libheap/frontend/commands/gdb/heap.py
@@ -2,26 +2,22 @@ from __future__ import print_function
 
 import sys
 
+from libheap.frontend.printutils import print_error, print_header
+from libheap.ptmalloc.malloc_state import malloc_state
+from libheap.ptmalloc.ptmalloc import ptmalloc
+
 try:
     import gdb
 except ImportError:
     print("Not running inside of GDB, exiting...")
     sys.exit()
 
-from libheap.frontend.printutils import print_header
-from libheap.frontend.printutils import print_error
-
-from libheap.ptmalloc.ptmalloc import ptmalloc
-
-from libheap.ptmalloc.malloc_state import malloc_state
-
 
 class heap(gdb.Command):
     """libheap command help listing"""
 
     def __init__(self, debugger=None, version=None):
-        super(heap, self).__init__("heap", gdb.COMMAND_OBSCURE,
-                                   gdb.COMPLETE_NONE)
+        super(heap, self).__init__("heap", gdb.COMMAND_OBSCURE, gdb.COMPLETE_NONE)
 
         if debugger is not None:
             self.dbg = debugger
@@ -34,24 +30,23 @@ class heap(gdb.Command):
     def invoke(self, arg, from_tty):
         # XXX: self.dbg.string_to_argv
         if arg.find("-h") != -1:
-            # print_header("heap ", end="")
-            # print("Options:", end="\n\n")
-            # print_header("{:<15}".format("-a 0x1234"))
-            # print("Specify an arena address")
-            print_header("{:<15}".format("heapls"))
+            print_header("{:<20}".format("heap [-a 0x1234]"), end="")
+            print("Print main_arena or arena at specified address")
+            print_header("{:<20}".format("heapls"))
             print("Print a flat listing of all chunks in an arena")
-            print_header("{:<15}".format("fastbins [#]"))
+            print_header("{:<20}".format("fastbins [#]"))
             print("Print all fast bins, or only a single fast bin")
-            print_header("{:<15}".format("smallbins [#]"))
+            print_header("{:<20}".format("smallbins [#]"))
             print("Print all small bins, or only a single small bin")
-            print_header("{:<15}".format("freebins"))
+            print_header("{:<20}".format("freebins"))
             print("Print compact bin listing (only free chunks)")
-            print_header("{:<15}".format("heaplsc"))
+            print_header("{:<20}".format("heaplsc"))
             print("Print compact arena listing (all chunks)")
-            print_header("{:<15}".format("mstats"), end="")
+            print_header("{:<20}".format("mstats"), end="")
             print("Print memory alloc statistics similar to malloc_stats(3)")
             # print_header("{:<22}".format("print_bin_layout [#]"), end="")
             # print("Print the layout of a particular free bin")
+            print("Currently configured for version: {}".format(self.version))
             return
 
         ptm = ptmalloc(self.dbg)
@@ -59,18 +54,26 @@ class heap(gdb.Command):
         if ptm.SIZE_SZ == 0:
             ptm.set_globals()
 
-        # XXX: from old heap command, replace
-        main_arena = self.dbg.read_variable("main_arena")
-        # XXX: add arena address guessing via offset without symbols
-        arena_address = self.dbg.format_address(main_arena.address)
-        ar_ptr = malloc_state(arena_address, debugger=self.dbg,
-                              version=self.version)
+        try:
+            main_arena = self.dbg.read_variable("main_arena")
+            arena_address = main_arena.address
+        except RuntimeError:
+            print("No gdb frame is currently selected.")
+            return
+        except (AttributeError, ValueError):
+            try:
+                res = gdb.execute("x/x &main_arena", to_string=True)
+                arena_address = int(res.strip().split()[0], 16)
+            except gdb.error:
+                print("WARNING: Debug glibc was not found.")
+                return
+
+        ar_ptr = malloc_state(arena_address, debugger=self.dbg, version=self.version)
 
         # XXX: add arena address passing via arg (-a)
         if (len(arg) == 0) and (ar_ptr.next == 0):
             # struct malloc_state may be invalid size (wrong glibc version)
-            print_error("No arenas could be found at {:#x}".format(
-                        ar_ptr.address))
+            print_error("No arenas could be found at {:#x}".format(ar_ptr.address))
             return
 
         print("Arena(s) found:", end="\n")
@@ -80,14 +83,16 @@ class heap(gdb.Command):
         if ar_ptr.address != ar_ptr.next:
             # we have more than one arena
 
-            curr_arena = malloc_state(ar_ptr.next, debugger=self.dbg,
-                                      version=self.version)
+            curr_arena = malloc_state(
+                ar_ptr.next, debugger=self.dbg, version=self.version
+            )
 
-            while (ar_ptr.address != curr_arena.address):
+            while ar_ptr.address != curr_arena.address:
                 print("  arena @ ", end="")
                 print_header("{:#x}".format(int(curr_arena.address)), end="\n")
-                curr_arena = malloc_state(curr_arena.next, debugger=self.dbg,
-                                          version=self.version)
+                curr_arena = malloc_state(
+                    curr_arena.next, debugger=self.dbg, version=self.version
+                )
 
                 if curr_arena.address == 0:
                     print_error("No arenas could be correctly found.")
--- libheap/pyptmalloc.py
+++ libheap/pyptmalloc.py
@@ -1,22 +1,40 @@
 import os
 import sys
 
+# allow someone to manually import the module if they didn't actually install
+# it using pip
+try_reimport = False
+try:
+    import libheap
+except ImportError:
+    module_path = os.path.dirname(os.path.abspath(__file__)) + "/../"
+    if module_path not in sys.path:
+        sys.path.insert(0, module_path)
+    print(sys.path)
+    try_reimport = False
+
+if try_reimport:
+    try:
+        import libheap
+    except ImportError:
+        print("Couldn't find libheap. Nothing to do")
+
+try:
+    from libheap.frontend import frontend_gdb, frontend_gdb_pretty_printers
+    from libheap.pydbg.debugger import pydbg
+    from libheap.pydbg.pygdbpython import pygdbpython
+except ImportError:
+    pass
+
 try:
     import gdb
 except ImportError:
     print("Not running inside of GDB, exiting...")
-    sys.exit()
 
 try:
     import configparser  # py3
 except:
     import ConfigParser as configparser  # py2
-
-from libheap.frontend import frontend_gdb
-from libheap.frontend import frontend_gdb_pretty_printers
-
-from libheap.pydbg.debugger import pydbg
-from libheap.pydbg.pygdbpython import pygdbpython
 
 
 class pyptmalloc:
@@ -33,7 +51,7 @@ class pyptmalloc:
 
         # Register GDB Commands
         frontend_gdb.frontend_gdb(self.debugger, self.glibc_version)
-
+ 
         # Register GDB Pretty Printers
         pp = frontend_gdb_pretty_printers.pretty_print_heap_lookup
         gdb.pretty_printers.append(pp)
